3957ea8dfd055ea9b51dd24c5bebcb99ff9509d3
Arnavkheterpal12 <71542068+Arnavkheterpal12@users.noreply.github.com>
2020-11-23 20:54:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(550,480);
+canvas.position(110,250);
+video=createCapture(VIDEO);
+video.hide();
+tint_color="";
+}
+function draw(){
+    image(video,0,0,550,480);
+    tint(tint_color);
+}
+function take_snapshot(){
+    save('YourPHOTO.png');
+
+}
+function filter_tint(){
+    tint_color=document.getElementById("add_filter").value;
+}
